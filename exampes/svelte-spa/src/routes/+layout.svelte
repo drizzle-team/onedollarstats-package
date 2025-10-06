883ad973d9350675fdc6ffff47de282421bca7aa
David Hordiienko <shiraz0david@gmail.com>
2025-10-06 14:54:49 +0300

    add: svlelte examples of configure

---
+++ exampes/svelte-spa/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import { onMount } from "svelte";
+  import { configure } from "onedollarstats";
+
+  let { children } = $props();
+
+  configure({ trackLocalhostAs: "example.com" });
+</script>
+
+<svelte:head>
+  <link
+    rel="icon"
+    href={favicon}
+  />
+</svelte:head>
+
+{@render children?.()}
---
+++ exampes/svelte-ssr/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import { onMount } from "svelte";
+  import { configure } from "onedollarstats";
+
+  let { children } = $props();
+
+  onMount(() => {
+    configure({ trackLocalhostAs: "example.com" });
+  });
+</script>
+
+<svelte:head>
+  <link
+    rel="icon"
+    href={favicon}
+  />
+</svelte:head>
+
+{@render children?.()}
